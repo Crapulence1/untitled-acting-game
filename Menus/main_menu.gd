0b608f3f194a485900473fe98bb4fa8bb06b775f
Crapulence1 <andrewkabatsky@gmail.com>
2025-06-09 23:19:02 -0400

    Init Main Menu
    made arbitrary main menu for beginning development

---
+++ Menus/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+
+
+
+func _on_play_pressed() -> void:
+	var new_scene = load("res://Menus/test.tscn")
+	get_tree().change_scene_to_packed(new_scene)
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
